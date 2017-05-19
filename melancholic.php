fe12082effe3ebe0ee1e86160af5bfc227f64584
Aino1993 <ojala@mail.com>
2017-05-19 14:57:56 +0300

    Files imported
    All the app's files added.

---
+++ melancholic.php
@@ -0,0 +1,38 @@
+ <head>
+<title>MusicRecApp</title>
+ <link rel="stylesheet" href="css/bootstrap.css">
+ <link rel="stylesheet" href="css/style.css">
+  <script src="js/bootstrap.min.js"></script>
+ </head><center><h1><?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "music_app";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM songs WHERE type='sad' ORDER BY rand() LIMIT 1";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo  $row["name"]. "<br>";
+  
+?>
+</h1>
+ <iframe width="300" height="225" src="<?php echo  $row["ytlink"] ?>">
+</iframe> 
+<?php
+    }
+} else {
+	echo "0 results";
+}
+mysqli_close($conn);?>
+<br><a href="home.php"><img src="images/back.png"></a><a href="melancholic.php"><img src="images/refresh.png"></a>
+</center>
